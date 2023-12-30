013b6f5d6803dc305990483485aee535b152e7da
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-30 03:25:17 -0400

    feat:retorno Login

---
+++ desafio-back-indt/Models/RetornoLogin.cs
@@ -0,0 +1,12 @@
+﻿using desafio_back_indt.Enums;
+
+namespace desafio_back_indt.Models
+{
+    public class RetornoLogin
+    {
+
+        public string Nome { get; set; }
+        public NivelAcessoEnum NivelAcesso { get; set; }
+
+    }
+}
